9121a80309375ce9e0f5c429db927e1188200c81
Harshavrthini MK <harshavarthinimk7201@gmail.com>
2026-03-26 22:13:50 +0530

    Update entrylevel.sh

--- entrylevel.sh
+++ entrylevel.sh
@@ -1,3 +1,4 @@
 echo " This file contains only basics of Day1"
 echo "This is poll scm method without build every minute jenkins check  git hub if has any changes or not without buildnow action"
 echo " Auto build sucess"
+echo "sucessully added webhook for advanced update than poll scm"
